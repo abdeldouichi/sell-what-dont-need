dd536dfa618403169cb322b996d765a217f7a9ba
abdel <abdel.douichi@gmail.com>
2021-06-26 14:38:03 +0200

    Add navigation

--- App.js
+++ App.js
@@ -2,18 +2,23 @@ import React, { useEffect} from 'react';
 import Screen from './app/components/Screen';
 import * as ImagePicker from "expo-image-picker"
 import { Text } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import AccountScreen from './app/screens/AccountScreen';
+import ListingDetailScreen from './app/screens/ListingDetailScreen';
+import ListingEditScreen from './app/screens/ListingEditScreen';
+import RegisterScreen from './app/screens/RegisterScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+const Drawer = createDrawerNavigator();
 export default function App(props) {
 
-    // const requestPermission = async()=>{
-    //     result = await ImagePicker.requestCameraRollPermissionsAsync();
-    //     if(!result.granted)
-    //         alert('You need to enable permission to acces to the library');
-    // }
-    // useEffect(()=>{
-    //     requestPermission();
-    // })
-    return (
-     <Screen><Text>hello</Text></Screen>
-    )
+    <NavigationContainer>
+    <Drawer.Navigator initialRouteName="AccountScreen">
+      <Drawer.Screen name="Account" component={AccountScreen} />
+      <Drawer.Screen name="Listing Detail" component={ListingDetailScreen} />
+      <Drawer.Screen name="Listing Edit" component={ListingEditScreen} />
+      <Drawer.Screen name="Register" component={RegisterScreen} />
+      <Drawer.Screen name="View Image" component={ViewImageScreen} />
+    </Drawer.Navigator>
+  </NavigationContainer>
 }
